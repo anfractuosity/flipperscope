519832c75d48f29f488de0a957a3554e7a9e5b78
anfractuosity <anfractuosity@anfractuosity.com>
2024-12-30 21:40:09 +0000

    Update about screen with version

--- scenes/scope_scene_about.c
+++ scenes/scope_scene_about.c
@@ -13,6 +13,7 @@ void scope_scene_about_on_enter(void* context) {
     FuriString* temp_str;
     temp_str = furi_string_alloc();
     furi_string_printf(temp_str, "\e#%s\n", "Information");
+    furi_string_cat_printf(temp_str, "Version: %s\n\n", FAP_VERSION);
     furi_string_cat_printf(
         temp_str,
         "Provide signal to pin 16/PC0, with a voltage ranging from 0V to 2.5V and ground to pin 18/GND.\n\n");
